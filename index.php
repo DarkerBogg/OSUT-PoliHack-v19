5452bb71a7218a251304437145c291a6013e4d76
DarkerBogg <57591431+DarkerBogg@users.noreply.github.com>
2026-04-24 23:35:47 +0300

    Indications commit
    Added indications for Iasmina.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <!-- <html lang="en"> -->
 <html>
 <head>
+	<!-- WARNING: add a name later -->
 	<title>[namespace]</title>
 
 	<meta charset="UTF-8">
@@ -22,7 +23,42 @@
 		<button id="nav-info">Info</button>
 	</nav>
 	<main id="content">
-		
+		<div id="home" class="page">
+			<h1>Ready to check out a new city?</h1>
+			<h2>Ever changing text.</h2>
+			<!--
+				TODO:
+				here (inside the comment), add prompts for the ever changing text.
+				it should say something different every few seconds
+
+				1. Look for a job that suits you
+				2. Study in the city you dreamt of
+				3. ...
+				4. ...
+				...
+			-->
+			<button id="goto-pick-cities" class="button">Get started</button>
+		</div>
+		<div id="pick-cities" class="page hidden">
+			<h1>Pick cities you want to explore</h1>
+			<!-- WARNING: might change in the future to dynamically add cards of cities entered -->
+			<h2>Just type in the names of the cities you want to learn more about, separated by commas.</h2>
+			<input type="text" id="search-cities" class="search-bar">
+		</div>
+		<!--
+			TODO:
+			add #pick-interests
+			add #results
+			-> as pages (.page.hidden)
+			-> only add a <h1></h1> tag
+		-->
+		<!--
+			TODO:
+			add #area-best-cities
+			add #city-best-areas
+			-> as pages (.page.hidden)
+			-> only add a <h1></h1> tag
+		-->
 	</main>
 </body>
 </html>
