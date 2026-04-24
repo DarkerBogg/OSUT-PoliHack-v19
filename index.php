d142d74994a81e6e0df6d0a50bdf5e98facfe13d
DarkerBogg <57591431+DarkerBogg@users.noreply.github.com>
2026-04-24 22:57:54 +0300

    Initial commit
    Added main page and various stylesheets.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<!-- <html lang="en"> -->
+<html>
+<head>
+	<title>[namespace]</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="./css/index.css">
+	<link rel="stylesheet" href="./css/nav.css">
+	<link rel="stylesheet" href="./css/content.css">
+</head>
+<body>
+	<nav id="nav">
+		<button id="nav-menu">Menu</button>
+		<button id="nav-edu">Education</button>
+		<button id="nav-work">Work</button>
+		<button id="nav-travel">Travel</button>
+		<div class="empty"></div>
+		<button id="nav-info">Info</button>
+	</nav>
+	<main id="content">
+		
+	</main>
+</body>
+</html>
